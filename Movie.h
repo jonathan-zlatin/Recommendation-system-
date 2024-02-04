e8196d077ea66b87f2288087d30b5a8a53d853a9
jonathan-zlatin <jonathanzlatin@gmail.com>
2024-02-04 21:34:57 +0200

    all project

--- Movie.h
+++ Movie.h
@@ -12,10 +12,7 @@ class Movie;
 
 typedef std::shared_ptr<Movie> sp_movie;
 
-/**
- * those declartions and typedefs are
- * given to you and should be used in the ex
- */
+
 typedef std::size_t (*hash_func) (const sp_movie &movie);
 typedef bool (*equal_func) (const sp_movie &m1, const sp_movie &m2);
 std::size_t sp_movie_hash (const sp_movie &movie);
